03bf5cba51219cb118e13ee2839986cf657c2080
konrad <konradpichalski@gmail.com>
2020-07-12 19:50:36 +0100

    Add api routes for authenticating and user

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/calendars', require('./routes/api/calendars'));
+app.use('/api/workouts', require('./routes/api/workouts'));
+app.use('/api/exercises', require('./routes/api/exercises'));
 
 const PORT = process.env.PORT || 5000;
 

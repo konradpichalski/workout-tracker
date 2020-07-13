63b5d722e0ebb67871a7919cbaa90390841c01b5
konrad <konradpichalski@gmail.com>
2020-07-13 11:23:44 +0100

    Add POST /exerciseplan api route to add a new exercise plan

--- routes/api/exercisePlan.js
+++ routes/api/exercisePlan.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
 const auth = require('../../middleware/auth');
 
 const ExercisePlan = require('../../models/ExercisePlan');
@@ -25,5 +27,42 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route     POST api/exerciseplan
+// @desc      Add exercise plan
+// @access    Private
+router.post(
+  '/',
+  [
+    auth,
+    check('name', 'Exercise name is required').not().isEmpty(),
+    check('sets', 'Sets are required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    // Validate required fields first and check for errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { name, sets } = req.body;
+
+      const newExercisePlan = new ExercisePlan({
+        user: req.user.id,
+        name,
+        sets,
+      });
+
+      const exercisePlan = await newExercisePlan.save();
+
+      res.json(exercisePlan);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
+  },
+);
 
 module.exports = router;

65c2c089a1e4e98a2d76f8f0654ede021797827f
konrad <konradpichalski@gmail.com>
2020-07-13 11:21:18 +0100

    Add GET /exerciseplan api route to fetch all exercise plans for the user

--- routes/api/exercisePlan.js
+++ routes/api/exercisePlan.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const ExercisePlan = require('../../models/ExercisePlan');
 
 // @route     GET api/exerciseplan
-// @desc      Test route
-// @access    Public
-router.get('/', (req, res) => res.send('ExercisePlan route'));
+// @desc      Get exercise plans
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const exercisePlans = await ExercisePlan.find({
+      user: req.user.id,
+    });
+
+    if (!exercisePlans) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no exercise plan for this user' });
+    }
+
+    res.json(exercisePlans);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;

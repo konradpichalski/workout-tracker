717c85f5522444b6307f079e0c1279fe08fc1a03
konrad <konradpichalski@gmail.com>
2020-07-13 11:42:05 +0100

    Add GET and POST trainingplan/:id api routes

--- routes/api/TrainingPlan.js
+++ routes/api/TrainingPlan.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 const auth = require('../../middleware/auth');
+const checkObjectId = require('../../middleware/checkObjectById');
 
 const TrainingPlan = require('../../models/TrainingPlan');
 
@@ -60,6 +61,72 @@ router.post(
       res.json(newTrainingPlan);
     } catch (err) {
       console.error(err);
+      res.status(500).send('Server error');
+    }
+  },
+);
+
+// @route     GET api/trainingplan/:id
+// @desc      Get training plan by id
+// @access    Private
+router.get('/:id', [auth, checkObjectId('id')], async (req, res) => {
+  try {
+    // find a training plan with id for the right user
+    const trainingPlan = await TrainingPlan.findOne({
+      user: req.user.id,
+      _id: req.params.id,
+    });
+
+    if (!trainingPlan) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no training plan for this user with this id' });
+    }
+
+    res.json(trainingPlan);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     POST api/trainingplan/:id
+// @desc      Update training plan by id
+// @access    Private
+router.post(
+  '/:id',
+  [
+    auth,
+    checkObjectId('id'),
+    check('name', 'Training plan name is required').not().isEmpty(),
+    check('exercises', 'Exercises are required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    // Validate required fields first and check for errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const trainingPlanData = {
+      user: req.user.id,
+      name: req.body.name,
+      _id: req.params.id,
+      exercises: req.body.exercises,
+    };
+
+    try {
+      // User upsert option (creates new doc if no match is found)
+      const updatedTrainingPlan = await TrainingPlan.findOneAndUpdate(
+        { user: req.user.id, _id: req.params.id },
+        { $set: trainingPlanData },
+        { new: true, upsert: true },
+      );
+
+      res.json(updatedTrainingPlan);
+    } catch (err) {
+      console.error(err.message);
       res.status(500).send('Server error');
     }
   },
